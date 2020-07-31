2fe58a6f34eb4b1538b6d3f7430bb8aec90cd803
Apple <ns5621172gmail.com>
2020-07-31 23:12:57 +0530

    changes

---
+++ docs/precache-manifest.2e53b01573848bf25a565a80ca15cbdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6feca43c45130d831ee64e2177978c4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d9dfc1fd7c17343ebb1",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "f6902ccc8c65f3ff3043",
+    "url": "/static/css/main.8863c256.chunk.css"
+  },
+  {
+    "revision": "3d9dfc1fd7c17343ebb1",
+    "url": "/static/js/2.822b73a8.chunk.js"
+  },
+  {
+    "revision": "f6902ccc8c65f3ff3043",
+    "url": "/static/js/main.0be28827.chunk.js"
+  },
+  {
+    "revision": "867559b179ea8c7af62a",
+    "url": "/static/js/runtime-main.6a03bc6f.js"
+  }
+]);
